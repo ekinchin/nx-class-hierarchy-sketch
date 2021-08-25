a87c991a6e47f4ec0325c6e1bc0cec06c01c84ca
ekinchin <evgen4in@gmail.com>
2021-08-25 12:11:57 +0500

    FIX ContainerType broken import

--- libs/container-lib/src/lib/containers/ContainerFactory.ts
+++ libs/container-lib/src/lib/containers/ContainerFactory.ts
@@ -1,12 +1,12 @@
-import { ContainerType } from './ContainerType';
 import containersType from './entity';
 import { AbstractContainerType } from './AbstractContainerType';
 import { IPackageList } from '../packageList';
 import { IContainerListItem } from '../containerList';
+import { IContainerType } from './IContainerType';
 
 export class ContainerFactory{
   create(
-    container: ContainerType,
+    container: IContainerType,
     packageList: IPackageList,
     containerList: IContainerListItem[]
   ): AbstractContainerType {
