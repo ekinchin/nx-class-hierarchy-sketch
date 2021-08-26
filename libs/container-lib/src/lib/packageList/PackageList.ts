2467a6bc68fd6652e1fa747f9ee8d8dd128e7076
ekinchin <evgen4in@gmail.com>
2021-08-26 11:26:59 +0500

    FIX: incorrect work with list - date drop from list when removed action. Date modified now - set field extractedAt

--- libs/container-lib/src/lib/packageList/PackageList.ts
+++ libs/container-lib/src/lib/packageList/PackageList.ts
@@ -2,39 +2,28 @@ import { IPackageList } from './IPackageList';
 import { IPackageListItem } from './IPackageListItem';
 
 export class PackageList implements IPackageList {
-  private _commands: [{
-    command: 'Append' | 'Remove',
-    packageItem: IPackageListItem,
-  }];
-
   constructor(
     private _packageList: IPackageListItem[],
-    private _code: string) { }
+    private _code: string
+  ) { }
 
   get packageListItems(): IPackageListItem[] {
-    const result = [...this._packageList];
-    for (const {command, packageItem } of this._commands) {
-      if(command === 'Append') result.push(packageItem);
-      if(command === 'Remove') result.filter(({name}) => name === packageItem.name);
-    }
-    return result;
-  }
+    return this._packageList;
+  };
 
   append(item: string): IPackageListItem | Error {
-    const isAlreadyExists = this.packageListItems.some(({ name }) => name === item);
+    const isAlreadyExists = this._packageList.some(({ name }) => name === item);
     if (isAlreadyExists) return new Error('package is exists already');
     const insertedAt = (new Date()).toISOString();
     const packageItem = { code: this._code, name: item, insertedAt };
-    this._commands.push({ command: 'Append', packageItem: packageItem });
+    this._packageList.push(packageItem);
     return packageItem;
   }
 
   remove(item: string): IPackageListItem | Error{
-    const existsItem = this.packageListItems.find(({ name }) => name === item);
-    if (!existsItem) return new Error('package is not exists');
-    const extractedAt = (new Date()).toISOString();
-    const packageItem = { ...existsItem, extractedAt };
-    this._commands.push({ command: 'Remove', packageItem: packageItem });
-    return packageItem;
+    const index = this._packageList.findIndex(({ name }) => name === item);
+    if (index === -1) return new Error('package is not exists');
+    this._packageList[index].extractedAt = (new Date()).toISOString();
+    return this._packageList[index];
   }
 }
